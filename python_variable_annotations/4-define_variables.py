98d1e87ad943611e4b8db8121863f88445b03231
scotty800 <scottyndanga02400@gmail.com>
2024-12-02 15:54:13 +0100

    Define variables

---
+++ python_variable_annotations/4-define_variables.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+
+# Variable entière. Respecte les conventions d'espacement de la PEP 8.
+a = 1
+
+# Variable flottante. Conforme à la PEP 8.
+pi = 3.14
+
+# Variable booléenne. Respecte les conventions de la PEP 8.
+i_understand_annotations = True
+
+# Variable annotée avec un type explicite (str), conforme à la PEP 8.
+school: str = "Holberton"
