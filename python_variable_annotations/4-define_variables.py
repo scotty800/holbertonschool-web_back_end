2fd10c8489abcef5e4d2142418bd1a6ee9a8863e
scotty800 <scottyndanga02400@gmail.com>
2024-12-02 16:00:53 +0100

    fix code

--- python_variable_annotations/4-define_variables.py
+++ python_variable_annotations/4-define_variables.py
@@ -1,13 +1,9 @@
 #!/usr/bin/env python3
 
-# Defining 'a' as an integer with the value 1
+"""4. Define variables"""
+
+
 a: int = 1
-
-# Defining 'pi' as a float with the value 3.14
 pi: float = 3.14
-
-# Defining 'i_understand_annotations' as a boolean with the value True
 i_understand_annotations: bool = True
-
-# Defining 'school' as a string with the value "Holberton"
 school: str = "Holberton"
