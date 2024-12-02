d372f64bc9059ae4c53353a58c42d47021a6b710
scotty800 <scottyndanga02400@gmail.com>
2024-12-02 15:40:24 +0100

    2-floor.py

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Returns the floor of a floating-point number."""
+
+
+import math
+
+
+def floor(n: int) -> float:
+    """Returns the floor of a floating-point number."""
+    return math.floor(n)
