2056bde2bfa7e81e898dc0db6cd36711130ff94c
scotty800 <scottyndanga02400@gmail.com>
2024-12-03 14:47:52 +0100

    pycodestyle

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -5,11 +5,19 @@
 import asyncio
 from typing import List
 
+
 get = __import__('3-tasks').task_wait_random
 
-
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    ''' Function that returns a list
+    ''' Fonction qui retourne une liste de délais en ordre croissant
+    en utilisant des tâches asyncio.
+    
+    Args:
+        n (int): Le nombre de fois où appeler task_wait_random.
+        max_delay (int): Le délai maximal en secondes pour chaque tâche.
+        
+    Returns:
+        List[float]: Liste des délais en ordre croissant.
     '''
     lst = [get(max_delay) for i in range(n)]
     finish = [await task for task in asyncio.as_completed(lst)]
