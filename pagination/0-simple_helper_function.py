2ede3dd0a434eb18ee38531dac7db96802aaccbb
scotty800 <scottyndanga02400@gmail.com>
2024-12-10 12:18:49 +0100

    0. Simple helper function

---
+++ pagination/0-simple_helper_function.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+"""Function index range"""
+
+
+def index_range(page, page_size):
+    """This function calculates the start and end index,
+    for a given page number and page size."""
+    start = (page - 1) * page_size
+    end = page * page_size
+    return tuple([start, end])
